78595a34736cf6d9f19eedf89c662ffda9e446df
PonomarevIK <PonomarevIK99@yandex.ru>
2022-04-23 21:22:29 +0300

    Game values balancing, bug fixes, preparation for multiplayer availability

--- constants.py
+++ constants.py
@@ -1,12 +1,11 @@
 # Constants
-WND_WIDTH = 900
-WND_HEIGHT = 700
-WND_CENTER = (WND_WIDTH//2, WND_HEIGHT//2)
 GAME_TITLE = "Drawl Stars"
-PLAYER_SPEED = 5
-BULLET_SPEED = 0.4
-BULLET_LENGTH = 50
-BULLET_COOLDOWN = 60
-WALL_MAX_LENGTH = 140
-WALL_COLOR_INACTIVE = (150, 150, 150)
-WALL_COLOR_ACTIVE = (165, 42, 42)
+WND_WIDTH = 900  # (in pixels)
+WND_HEIGHT = 700  # (in pixels)
+PLAYER_SPEED = 5  # (in pixels)
+BULLET_SPEED = 0.4  # how fast a bullet travels relative to its length (pixels moved / bullet length)
+BULLET_COOLDOWN = 40  # time until player can shoot again (in frames)
+BULLET_LENGTH = 50  # length of line representing a bullet (in pixels)
+WALL_MAX_LENGTH = 200  # (in pixels)
+WALL_COLOR_INACTIVE = (150, 150, 150)  # (RGB)
+WALL_COLOR_ACTIVE = (165, 42, 42)  # (RGB)
