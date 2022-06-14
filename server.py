41deafced6bcbc770a2ea0cf8bf5bfe2f6b65a64
Ivan <PonomarevIK99@yandex.ru>
2022-06-14 15:06:31 +0500

    Network class can now process multiple actions sent in one frame. Reduced over-indentation in some "if ... if ... if" statements

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def client_thread(connection, address, client_id):
             client_last_message[client_id] = data
             for id, message in client_last_message.items():
                 if id != client_id:
-                    connection.send(f"{id}:".encode() + message)
+                    connection.send(f"{id}-".encode() + message)
         except socket.error as error:
             print(error)
             break
